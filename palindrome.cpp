0f59ec73b7e9843d118e9a753b3471b8d127a1c1
U-BSD\517459 <517459@SHS-15W2NW3.beaverton.k12.or.us>
2025-09-08 10:12:28 -0700

    reverse is doing something weird

--- palindrome.cpp
+++ palindrome.cpp
@@ -4,36 +4,76 @@
 int main()
 {
   //Holds allowed characters
-  char allowedChars[52] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-			   'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-			   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-			   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
+  char allowedCharsUpper[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+				'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+  char allowedCharsLower[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+			        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}; 
 
   //Takes the input text
   char input[80];
-  cin >> input;
+  std::cin.get(input, 80);
 
   char formattedInput[80];
-  //Removes everything not in allowedChars
-  for(i = 0, i <= 80; i++)
-  {
+  int placedCounter = 0;
     
-  }
-
-  char reverse[81];
-  for(i = 81; i >= 0; i--)
+  //Removes everything not in allowedChars
+  for(int i = 0; i <= 80; i++)
   {
+
+    //Debug
+    std::cout << "Validating" << input[i] << placedCounter << "\n";
+    
+    //Checks for termination
     if(input[i] == '\0')
     {
-      for(j = i; i >= 0; i--)
-      {
-	if(strchr(allowedChars, input[i] != nullptr))
-	{
-	  reverse[81-i] = input[i]
-	}
-      }
-	
+      formattedInput[placedCounter] = '\0';
+      break;
+    }
+
+    //Uppercase, go to lower
+    else if(strchr(allowedCharsUpper, input[i]) != nullptr)
+    {
+      formattedInput[placedCounter] = input[i] + 32;
+      placedCounter++;
+    }
+
+    //Lowercase, continue
+    else if(strchr(allowedCharsLower, input[i]) != nullptr)
+    {
+      formattedInput[placedCounter] = input[i];
+      placedCounter++;
+    }
   }
-  
+
+  //Debug
+  std::cout << "Formatted input" << formattedInput;
+
+  //Iterate from back to find terminator, then start there
+  char reverse[80];
+  for(int i = 81; i >= 0; i--)
+  {
+    if(formattedInput[i] == '\0')
+    {
+      for(int j = i - 1; j >= 0; j--)
+      {
+	reverse[i - j] = formattedInput[j];
+      }
+      reverse[i] = '\0';
+      break;
+    }
+  }
+
+  //Debug
+  std::cout << "Reversed" << reverse;
+
+  //Check if palindrome
+  bool isPalindrome = false;
+  if(strcmp(formattedInput, reverse) == 0)
+  {
+    isPalindrome = true;
+  }
+
+  //Print out
+  if(isPalindrome == true) { std::cout << "Palindrome"; }
+  else { std::cout << "Not a palindrome"; }
 }
-      
